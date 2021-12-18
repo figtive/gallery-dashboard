8174ccaf825d083812fb6e4b2281eb8ab31b6ae1
ilmannafian04 <milmannafian04@gmail.com>
2021-12-18 12:38:13 +0700

    Revert "Revert "build: added base path setting""
    This reverts commit 21b0e2ad9e804d7375a5d56abfa3c2c774252270.

--- svelte.config.js
+++ svelte.config.js
@@ -18,6 +18,10 @@ const config = {
     }),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    trailingSlash: 'ignore',
+    paths: {
+      base: '/admin',
+    },
   },
 };
 
